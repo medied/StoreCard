ea8a0fc6b2def17e6cb66285b0e5e1d8033ef717
Andrew Green Harris <agharris73@gmail.com>
2014-10-08 16:27:08 -0600

    Initial Commit

---
+++ StoreCard/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  StoreCard
+//
+//  Created by Andrew Harris on 10/8/14.
+//  Copyright (c) 2014 mercurypay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
---
+++ StoreCard/SecondViewController.m
@@ -0,0 +1,27 @@
+//
+//  SecondViewController.m
+//  StoreCard
+//
+//  Created by Andrew Harris on 10/8/14.
+//  Copyright (c) 2014 mercurypay. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
